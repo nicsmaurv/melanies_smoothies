2210fbe3fef8ca3e8111ae1163b5ab1d7fe3d974
nicsmaurv <nyxmaur.games@gmail.com>
2025-05-08 21:11:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ st.title("Customized Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
